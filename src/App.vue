efc210caee1c9a6c6f70fc2056c6bb946e9e3184
Cain Hall <cain.hall98@gmail.com>
2018-08-03 09:14:27 +1000

    Vue cli starter init, scss setup, create nav component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Nav msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Nav
   }
 }
 </script>
@@ -22,7 +21,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="nav">
+    <div class="nav-header">
+      <!-- <h3>Brightness & Contrast Developer Test</h3> -->
+      <p>{{date}}</p>
+    </div>
+    <div>
+      <img class="nav-image" src="../assets/beyond-the-valley.png" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      date: new Date().toString().split(' ').splice(1,3).join(' ') // get simple date,
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .nav {
+    max-width: 375px;
+    color: #fff;
+    background: $c-main-bg;
+    margin: 0px;
+    padding: 0px;
+    &-header > h3, p {
+      margin: 0px;
+    }
+    &-image {
+      width: 100%;
+      margin: 0px;
+    }
+  }
+</style>
